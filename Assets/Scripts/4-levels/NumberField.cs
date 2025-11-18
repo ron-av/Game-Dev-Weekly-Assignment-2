d872f25d5dece57af76a5f2000513255c8aa68e0
Ron <ronavraham1999@gmail.com>
2025-11-18 13:46:12 +0200

    commit5

--- Assets/Scripts/4-levels/NumberField.cs
+++ Assets/Scripts/4-levels/NumberField.cs
@@ -31,37 +31,3 @@ public class NumberField : MonoBehaviour
         SetNumber(number + toAdd);
     }
 }
-
-
-
-/*
-using TMPro;
-using UnityEngine;
-
-
-
- 
-[RequireComponent(typeof(TextMeshPro))]
-
-
-public class NumberField : MonoBehaviour
-{
-    private int number;
-
-    public int GetNumber()
-    {
-        return number;
-    }
-
-    public void SetNumber(int newNumber)
-    {
-        number = newNumber;
-        GetComponent<TextMeshPro>().text = newNumber.ToString();
-    }
-
-    public void AddNumber(int toAdd)
-    {
-        SetNumber(number + toAdd);
-    }
-}
-*/
